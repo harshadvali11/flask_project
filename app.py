1956d3b78c7884f0b5ec42af613cc6c1da808a53
harshad116 <harshadvali1154@gmail.com>
2020-12-04 20:54:44 -0800

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+
+from flask_wtf import Form
+
+from wtforms import StringField,SubmitField
+
+from wtforms.validators import Required
 
 app=Flask(__name__)
+
+app.config['SECRET_KEY']='encryption'
 
 
 @app.route('/function1/')
@@ -14,6 +22,38 @@ def template():
 @app.route('/wish/<name>')
 def wish(name):
     return 'hai hello how are U MR/MS {}'.format(name)
+
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        form_data=request.form
+        return form_data['username']
+
+    return render_template('form.html')
+
+
+class NameForm(Form):
+    name=StringField('Enter name',validators=[Required()])
+    submit=SubmitField()
+
+@app.route('/webform',methods=['GET',"POST"])
+def webform():
+    form=NameForm()
+    if form.validate_on_submit():
+        return form.data['name']
+    return render_template('webform.html',form=form)
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
